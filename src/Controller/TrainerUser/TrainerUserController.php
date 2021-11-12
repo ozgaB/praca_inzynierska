7cf4d7a489d757b6258d03b1fdeda430cf17b00d
BartekO <sqidix@gmail.com>
2021-11-12 18:40:45 +0100

    12.11.2021

--- src/Controller/TrainerUser/TrainerUserController.php
+++ src/Controller/TrainerUser/TrainerUserController.php
@@ -1,8 +1,10 @@
 <?php
 namespace App\Controller\TrainerUser;
 
+use App\Entity\Security\User;
 use App\Repository\InvitationRepository;
 use App\Repository\ParticipantRepository;
+use App\Repository\UserRepository;
 use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
@@ -39,7 +41,35 @@ class TrainerUserController extends AbstractController
         ];
     }
 
-    
+    /**
+     * @Route("/show_all_plans_for_user/{user}", name="show_all_plan_for_user")
+     * @Template()
+     */
+    public function showAllPlansForUser(User $user, Security $security, ParticipantRepository $participantRepository)
+    {
+        $trainerId = $security->getUser()->getId();
+        $userId = $user->getId();
+        $participantsWithPlans = $participantRepository->getParticipantWithAccessPlan($trainerId,$userId);
+
+        return [
+            'participant' => $user,
+            'participantsWithPlans' => $participantsWithPlans
+        ];
+    }
+
+    /**
+     * @Route("/show_all_plans_for_participants", name="show_all_plan_for_participants")
+     * @Template()
+     */
+    public function showAllPlansForParticipants(User $user, Security $security, ParticipantRepository $participantRepository)
+    {
+        $trainerId = $security->getUser()->getId();
+        $participantsWithPlans = $participantRepository->getParticipantsWithAccessPlan($trainerId);
+
+        return [
+            'participantsWithPlans' => $participantsWithPlans
+        ];
+    }
 }
 
 
--- src/Repository/ParticipantRepository.php
+++ src/Repository/ParticipantRepository.php
@@ -63,8 +63,8 @@ class ParticipantRepository extends ServiceEntityRepository
         ])
         ->from('App\Entity\Participant\Participant','participant')
         ->innerJoin('App\Entity\Security\User','user', Join::WITH, 'user.id = participant.idUser')
-        ->leftJoin('App\Entity\TrainingPlan\TrainingPlanAccess','access', Join::WITH, ':id_training_plan = access.idTrainingPlan')
-        ->where($qb->expr()->andX($qb->expr()->eq('participant.idTrainer',':id_trainer'),$qb->expr()->isNull('access.idUser')))
+        ->leftJoin('App\Entity\TrainingPlan\TrainingPlanAccess','access', Join::WITH,$qb->expr()->andX($qb->expr()->eq(':id_training_plan','access.idTrainingPlan'),$qb->expr()->eq('access.idUser','participant.idUser')))
+        ->where($qb->expr()->andX($qb->expr()->eq('participant.idTrainer',':id_trainer')))
         ->setParameter(':id_trainer',$trainerId)
         ->setParameter(':id_training_plan',$trainingPlanId);
 
@@ -84,6 +84,53 @@ class ParticipantRepository extends ServiceEntityRepository
         ->setParameter(':id_trainer',$trainerId);
 
         return $qb->getQuery()->getSingleScalarResult();
+    }
+
+    public function getParticipantsWithAccessPlan(int $trainerId)
+    {
+        $em = $this->getEntityManager();
+        $qb = $em->createQueryBuilder();
+        $qb
+        ->select([
+            'participant.idUser',
+            'user.firstName AS firstName',
+            'user.lastName AS lastName',
+            'trainingPlanList.planName AS planName',
+            'access.id AS idAccess',
+            'trainingPlanList.id AS idTrainingPlan',
+        ])
+        ->from('App\Entity\Participant\Participant','participant')
+        ->innerJoin('App\Entity\Security\User','user', Join::WITH, 'user.id = participant.idUser')
+        ->innerJoin('App\Entity\TrainingPlan\TrainingPlanAccess','access', Join::WITH, 'access.idUser = participant.idUser')
+        ->innerJoin('App\Entity\TrainingPlan\TrainingPlanList','trainingPlanList', Join::WITH, 'trainingPlanList.id = access.idTrainingPlan')
+        ->where($qb->expr()->eq('participant.idTrainer',':id_trainer'))
+        ->setParameter(':id_trainer',$trainerId);
+
+        return $qb->getQuery()->getResult();
+    }
+
+    public function getParticipantWithAccessPlan(int $trainerId, int $userId)
+    {
+        $em = $this->getEntityManager();
+        $qb = $em->createQueryBuilder();
+        $qb
+        ->select([
+            'participant.idUser',
+            'user.firstName AS firstName',
+            'user.lastName AS lastName',
+            'trainingPlanList.planName AS planName',
+            'access.id AS idAccess',
+            'trainingPlanList.id AS idTrainingPlan',
+        ])
+        ->from('App\Entity\Participant\Participant','participant')
+        ->innerJoin('App\Entity\Security\User','user', Join::WITH, 'user.id = :id_user')
+        ->innerJoin('App\Entity\TrainingPlan\TrainingPlanAccess','access', Join::WITH, 'access.idUser = participant.idUser')
+        ->innerJoin('App\Entity\TrainingPlan\TrainingPlanList','trainingPlanList', Join::WITH, 'trainingPlanList.id = access.idTrainingPlan')
+        ->where($qb->expr()->eq('participant.idTrainer',':id_trainer'))
+        ->setParameter(':id_trainer',$trainerId)
+        ->setParameter(':id_user', $userId);
+
+        return $qb->getQuery()->getResult();
     }
 
 }
